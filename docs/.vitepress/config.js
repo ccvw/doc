d80a896082c5d8286765ff623137e4e312fce866
Michael <asdhch@163.com>
2021-03-17 17:39:27 +0800

    add favicon

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,14 +2,18 @@ module.exports = {
   lang: 'zh-CN',
   title: '文档库',
   description: '个人文档中心',
+  head: [
+    [
+      'link',
+      { rel: 'shortcut icon', type: 'image/png', href: `../images/logo.png` },
+    ],
+  ],
   base: '/doc/',
 
   themeConfig: {
     repo: 'ichh/doc',
     docsDir: 'docs',
-    nav: [
-      { text: 'Web', link: '/web/index', activeMatch: '^/web/' },
-    ],
+    nav: [{ text: 'Web', link: '/web/index', activeMatch: '^/web/' }],
 
     sidebar: {
       '/web/': getWebSidebar(),
