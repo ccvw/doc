21d3623125357d39dd0056919b6d062f2805857a
Michael <asdhch@163.com>
2021-03-17 15:58:48 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  lang: 'zh-CN',
+  title: '文档库',
+  description: '个人文档中心',
+  base: '/doc/',
+
+  themeConfig: {
+    repo: 'ichh/doc',
+    docsDir: 'docs',
+    nav: [
+      { text: 'Web', link: '/web/index', activeMatch: '^/web/' },
+    ],
+
+    sidebar: {
+      '/web/': getWebSidebar(),
+    },
+  },
+}
+
+function getWebSidebar() {
+  return [
+    {
+      text: 'HTML',
+      link: '/web/index',
+      children: [
+        { text: '介绍', link: '/web/intro' },
+        { text: '开始', link: '/web/start' },
+      ],
+    },
+  ]
+}
